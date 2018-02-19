2db312fb2b6b0fda473a2e949a7c56434640dba2
Edwin Chu <edwinychu@gmail.com>
2018-02-18 21:58:32 -0800

    set up knex + bookshelf + scripts

--- src/database/index.js
+++ src/database/index.js
@@ -1,9 +1,10 @@
 const pg = require('pg');
+const pgCloudURL = require('../../config.js').pgCloudURL
 const dbPassword = require('../../config.js').dbPassword;
 const port = process.env.PORT || 5432;
 
 const connection =
-  `postgres://postgres:${dbPassword}@localhost:5432/tino` ||
+  `postgres://postgres:${dbPassword}@${pgCloudURL}:5432/tino` || `postgres://postgres:${dbPassword}@localhost:5432/tino` ||
   `${process.env.DATABASE_URL}`;
 
 const knex = require('knex')({
