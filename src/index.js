9e0a3bf6cf6264925766ef99a69586c947ffe542
Devon <devonyu415@gmail.com>
2018-02-22 13:46:24 -0800

    Revert "Feat: MVP React-Router implementation with User Price Prediction and Analysis onClick"

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 import React from 'react';
-import { render } from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App';
-import './index.css';
-import registerServiceWorker from './registerServiceWorker';
+import ReactDOM from 'react-dom';
+import { browserHistory } from 'react-router';
 
-render((
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-), document.getElementById('root'));
+import Routes from './routes';
+import registerServiceWorker from './utils/registerServiceWorker';
+
+ReactDOM.render(
+  <Routes history={browserHistory} />,
+  document.getElementById('root')
+);
 
 registerServiceWorker();
